b6d5c14cb6fcd053c2a122c585358c467d4744a3
flohms18 <trouilletflorent@outlook.com>
2026-05-14 22:17:26 +0200

    Understood Ref

--- src/HelloWorld.vue
+++ src/HelloWorld.vue
@@ -4,16 +4,24 @@ import { ref, onMounted } from 'vue';
 
 const info = ref([])
 
+const jackdaw = ref('James')
+
 onMounted(() => {
     axios
-        .get('https://restcountries.com/v3.1/name/peru')
+        .get('https://restcountries.com/v3.1/all?fields=name,capital,currencies')
         .then(response => { info.value = response.data })
+
+    jackdaw.value = 'Bond'
+
+    
 })
 
 </script>
 
 <template>
     {{  info }}
+
+    {{ jackdaw }}
 </template>
 
 <style scoped>
