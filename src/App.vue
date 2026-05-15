c5d8f056d479bbb235a6696ec59168072d9a700d
flohms18 <trouilletflorent@outlook.com>
2026-05-15 10:00:43 +0200

    Added Footer on App

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <script setup>
 
 import Header from './Header.vue';
+import Footer from './Footer.vue';
 import HelloWorld from './HelloWorld.vue';
 </script>
 
 <template>
   <Header></Header>
+  <HelloWorld></HelloWorld>
   <Footer></Footer>
 
-  <HelloWorld></HelloWorld>
+  
   
 </template>
 
