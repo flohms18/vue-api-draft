606402b6aee46ff84f9efcfdd4dc68b1fb9bf488
flohms18 <trouilletflorent@outlook.com>
2026-05-16 19:09:37 +0200

    Footer Style

--- src/Footer.vue
+++ src/Footer.vue
@@ -2,10 +2,24 @@
 </script>
 
 <template>
-
-    This is my footer
-
+  <footer class="footer">
+    <p class="footer-text">Made by F.T</p>
+  </footer>
 </template>
 
 <style scoped>
+.footer {
+  background-color: #0c0116;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 1rem;
+  width: 100%;
+}
+
+.footer-text {
+  color: #f8f9fa;
+  margin: 0;
+  font-size: 0.9rem;
+}
 </style>
