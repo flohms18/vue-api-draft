818591494a6906a1ec2c5cf09fc146929aa3c9dc
flohms18 <trouilletflorent@outlook.com>
2026-05-16 19:01:08 +0200

    Style Navbar

--- src/Header.vue
+++ src/Header.vue
@@ -3,20 +3,28 @@
 
 <template>
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Albus</a>
+<nav class="navbar navbar-expand-lg">
+  <a class="brand" href="#">Albus</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-  <div class="collapse navbar-collapse" id="navbarText">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">About</a>
-      </li>
-    </ul>
-  </div>
 </nav>
 </template>
 
 <style scoped>
+nav {
+  background-color: #0c0116;
+}
+
+.brand {
+  color: #5c6bff;
+  text-decoration: none;
+  font-size: 1.8rem;
+  padding-left: 1rem;
+}
+
+.brand:hover {
+  color: #5c6bff;
+  text-decoration: none;
+}
 </style>
